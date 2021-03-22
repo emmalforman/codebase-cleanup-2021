0ee5c5558fb90e430005d755d308c6406e85ba4b
emmalforman <73502597+emmalforman@users.noreply.github.com>
2021-03-22 15:16:41 -0700

    Testing Code

--- test/shopping_test.py
+++ test/shopping_test.py
@@ -1,4 +1,10 @@
 
-# TODO: import some code
+# TODO: import some code from the shopping file
+
+from app.shopping import format_usd
 
 # TODO: test the code
+def test_format_usd():
+   # assert 2==2
+   assert format_usd(9.5) == "$9.50"
+
