6ea4a7814550778d7a63f89f2f4c239184b23fe5
emmalforman <73502597+emmalforman@users.noreply.github.com>
2021-03-26 09:40:01 -0700

    test code

--- test/game_test.py
+++ test/game_test.py
@@ -15,3 +15,4 @@ def test_determination_of_the_winner():
     assert determine_winner("scissors", "rock") == "rock"
     assert determine_winner("scissors", "paper") == "scissors"
     assert determine_winner("scissors", "scissors") == None
+    #new line testing
